758692d1c2fe9a8b51d7a969e7867a8390d48a3b
Fullfifax <fullfifax1245@gmail.com>
2025-06-04 20:20:53 +0300

    ch-01: feat: update quote API; add and style new quote button

--- challenge-01-random-quote/assets/js/script.js
+++ challenge-01-random-quote/assets/js/script.js
@@ -1,24 +1,33 @@
 async function getQuote() {
+    const quoteEl = document.getElementById('quote');
+    const authorEl = document.getElementById('author');
+  
+    // Show loading message
+    quoteEl.innerText = "Loading quote...";
+    authorEl.innerText = "";
+  
     try {
-        const apiUrl = "https://zenquotes.io/api/random";
-        const cacheBuster = '?_=' + new Date().getTime();
-
-        // Avoid CORS restrictions
-        const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(apiUrl + cacheBuster);
-
-        const res = await fetch(proxyUrl);
-        if(!res.ok) throw new Error("Network response was not OK :(");
-
-        const data = await res.json();
-        const quotes = JSON.parse(data.contents);
-
-        document.getElementById('quote').textContent = `${quotes[0].q}`;
-        document.getElementById('author').textContent = `${quotes[0].a}`;
+      const apiUrl = "https://api.quotable.kurokeita.dev/api/quotes/random";
+      const cacheBuster = '?_=' + new Date().getTime();
+      
+      // Avoid CORS restrictions
+      const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(apiUrl + cacheBuster);
+  
+      const res = await fetch(proxyUrl);
+      if (!res.ok) throw new Error("Network response was not OK :(");
+  
+      const data = await res.json();
+      const quoteObj = JSON.parse(data.contents).quote;
+  
+      quoteEl.innerText = quoteObj.content;
+      authorEl.innerText = `— ${quoteObj.author.name}`;
     } catch (e) {
-        document.getElementById('quote').textContent = "Failed to load quote.";
-        document.getElementById('author').textContent = "";
-        console.error(e);
+      quoteEl.innerText = "Failed to load quote.";
+      authorEl.innerText = "";
+      console.error(e);
     }
-}
+  }
+
+document.getElementById("new-quote").addEventListener("click", getQuote);
 
 getQuote();
